02f606b0d7730b3dcbe0a3505fbe5d7b02ad93c1
ethanBonn <ethanbonn21@gmail.com>
2019-06-07 13:39:02 -0400

    saving

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ function back(){
   box.style.backgroundColor = 'green';
 }
 
+function homefix(){
+  home.style.fontWeight = 'bold';
+}
+
 let box = document.getElementById("box");
 box.addEventListener('mouseover', change);
 box.addEventListener('mouseout', back);
+window.onload =  homefix;
