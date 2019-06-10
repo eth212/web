70d54f8f48167a649ba9401942954535a1b02c0e
ethanBonn <ethanbonn21@gmail.com>
2019-06-09 23:00:30 -0400

    update

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 
+// creating the typing effect
+let target = document.getElementById("typing");
+let text = 'Hi, my name is Ethan and I am a Developer';
+i=0;
 
-
-
-
-
-
+function typer(){
+  if(i==0){
+    target.innerHTML = '<h1>';
+  };
+  if(0 < i < text.length - 1){
+    let buffer = text.charAt(i);
+    target.innerHTML +=  buffer ;
+    i++;
+    setTimeout(typer, 100);
+  };
+  if(i == text.length - 1){
+    target.innerHTML += text.charAt(i) + '</h1>';
+    i++;
+    setTimeout(typer, 100);
+  };
+};
+setTimeout(typer, 500);
 
 
 let home = document.getElementById("home");
