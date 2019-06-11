3aa9c1d98138a5e7a88a16aa5ed1847d779002bc
ethanBonn <ethanbonn21@gmail.com>
2019-06-11 15:54:07 -0400

    hash fix

---
+++ hashRouter.js
@@ -0,0 +1,23 @@
+class IndexView {
+  constructor(){
+    window.addEventListener("hashchange", e => this.onRouteChange(e));
+    this.slot = document.querySelector('#pageChange');
+  }
+  onRouteChange(e) {
+    const hashLocation = window.location.hash.substring(1);
+    console.log(hashLocation);
+    this.loadContent(hashLocation);
+  }
+
+  loadContent(uri){
+    var contentUri = `${uri}.html`;
+    console.log(contentUri);
+    fetch(contentUri).then(r => r.text()).then(content => this.updateSlot(content));
+
+  }
+
+  updateSlot(content){
+  this.slot.innerHTML = content;
+  }
+}
+new IndexView();
