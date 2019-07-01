e7f29082670e21f246141dc5f824e283ec91bfa4
ethanBonn <ethanbonn21@gmail.com>
2019-07-01 12:42:52 -0400

    test

--- hashRouter.js
+++ hashRouter.js
@@ -31,6 +31,23 @@ class IndexView {
         };
       };
       setTimeout(typer, 500);
+      let navHome = document.getElementById("Home");
+      let navAbout = document.getElementById("About");
+      let navProjects = document.getElementById("Projects");
+      let navContact = document.getElementById("Contact");
+
+      function displayName(element, word){
+        element.innerHTML = 'word';
+      }
+      function displayIcon(element, icon){
+        element.innerHTML = `<i class="fa fa-${icon}" ></i>`;
+
+      }
+
+
+      window.addEventListener('mouseover', displayName(navHome, 'home'));
+      window.addEventListener('mouseout', displayIcon(navHome, 'home'));
+      
 
     }
   }
