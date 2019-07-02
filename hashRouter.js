b9a2e8abd199fec547a6c302430564f6307e0591
eth212 <ethanbonn21@gmail.com>
2019-07-01 22:09:28 -0400

    testing

--- hashRouter.js
+++ hashRouter.js
@@ -10,27 +10,6 @@ class IndexView {
       console.log(hashLocation)}, 2000);
     this.loadContent(hashLocation);
     if(hashLocation == 'home'){
-      let target = document.getElementById("view");
-      let text = 'Hi, my name is Ethan and I am a Developer check';
-      let i=0;
-
-      function typer(){
-        if(i==0){
-          target.innerHTML = '<h1>';
-        };
-        if(0 < i < text.length - 1){
-          let buffer = text.charAt(i);
-          target.innerHTML +=  buffer ;
-          i++; 
-          setTimeout(typer, 100);
-        };
-        if(i == text.length - 1){
-          target.innerHTML += text.charAt(i) + '</h1>';
-          i++;
-          setTimeout(typer, 100);
-        };
-      };
-      setTimeout(typer, 500);
       let navHome = document.getElementById("Home");
       let navAbout = document.getElementById("About");
       let navProjects = document.getElementById("Projects");
