1165ca1ec36c0a7c466bf4b0529c5c051927988d
eth212 <ethanbonnardeaux@gmail.com>
2019-08-17 15:53:33 -0400

    route fix

--- hashRouter.js
+++ hashRouter.js
@@ -9,51 +9,9 @@ class IndexView {
     setTimeout(() => {
       console.log(hashLocation)}, 2000);
     this.loadContent(hashLocation);
-    if(hashLocation == 'home'){
-      let target = document.getElementById("view");
-      let text = 'Hi, my name is Ethan and I am a Developer check';
-      let i=0;
 
-      function typer(){
-        if(i==0){
-          target.innerHTML = '<h1>';
-        };
-        if(0 < i < text.length - 1){
-          let buffer = text.charAt(i);
-          target.innerHTML +=  buffer ;
-          i++;
-          setTimeout(typer, 100);
-        };
-        if(i == text.length - 1){
-          target.innerHTML += text.charAt(i) + '</h1>';
-          i++;
-          setTimeout(typer, 100);
-        };
-      };
-      setTimeout(typer, 500);
-      let navHome = document.getElementById("Home");
-      let navAbout = document.getElementById("About");
-      let navProjects = document.getElementById("Projects");
-      let navContact = document.getElementById("Contact");
-
-      function displayName(element, word){
-        element.innerHTML = 'word';
-      }
-      function displayIcon(element, icon){
-        element.innerHTML = `<i class="fa fa-${icon}" ></i>`;
-
-      }
-
-
-      window.addEventListener('mouseover', displayName(navHome, 'home'));
-      window.addEventListener('mouseout', displayIcon(navHome, 'home'));
-
-
-    }
-    else{
-      document.getElementById("view").innerHTML = "";
-    }
   }
+
   loadContent(uri){
     var contentUri = `${uri}.html`;
     console.log(contentUri);
