68619d7e7ead6a7a19162d9a7aa69b6639b9c85d
eth212 <ethanbonn21@gmail.com>
2019-09-27 19:42:04 -0400

    route fix

--- hashRouter.js
+++ hashRouter.js
@@ -1,3 +1,36 @@
+let target = document.getElementById("animation");
+let text = 'Hey, Im ethan welcome to my website';
+let i=0;
+let e = 0;
+
+
+function typer(){
+  if(i==0){
+    target.innerHTML = '<h1>';
+  };
+  if(0 < i < text.length - 1){
+    if( e == 0){
+      let buffer = text.charAt(i);
+      target.innerHTML +=  buffer ;
+      i++;
+      setTimeout(typer, 75);
+    }
+  };
+  if(i == text.length - 1){
+    target.innerHTML += text.charAt(i) + '</h1>';
+    i++;
+    setTimeout(typer, 75);
+  };
+  return false;
+};
+window.addEventListener("load", () => {typer()});
+window.addEventListener("load", () => {
+  window.location.href = "#home";
+});
+window.addEventListener("hashchange", () => {
+  e += 1;
+})
+
 class IndexView {
   constructor(){
     window.addEventListener("hashchange", e => this.onRouteChange(e));
@@ -7,6 +40,8 @@ class IndexView {
     var target;
     const hashLocation = window.location.hash.substring(1);
     console.log(hashLocation);
+
+    document.getElementById("animation").innerHTML = "<h1>" + hashLocation + "<h1>";
   //  target = document.getElementById(hashLocation);
     //setTimeout(() => {target.classList.add("clicked")},20 )
 
@@ -20,6 +55,7 @@ class IndexView {
     fetch(contentUri).then(r => r.text()).then(content => this.updateSlot(content));
 
   }
+
 
 
   updateSlot(content){
